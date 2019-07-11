81952a5089bea759f29d7922ff29ebb8396026ec
Lucas Volle <lvolle@exagoinc.com>
2019-07-11 12:44:06 -0400

    rename xml file to webreports.xml

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const util = require('util');
 xml2js.parseStringPromise = util.promisify(xml2js.parseString);
 
 async function main() {
-    let xml = fs.readFileSync('xml.xml');
+    let xml = fs.readFileSync('WebReports.xml');
     const parsedXml = await xml2js.parseStringPromise(xml);
     
     // console.log(parsedXml);
