7d8f20cb38338ef11c6c145875722fb88cef73df
Lucas Volle <lvolle@exagoinc.com>
2019-07-11 09:35:12 -0400

    read from file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const xml2js = require('xml2js');
+const fs = require('fs');
 const util = require('util');
 xml2js.parseStringPromise = util.promisify(xml2js.parseString);
 
 async function main() {
-    var xml = "<root>Hello xml2js!</root>"
+    let xml = fs.readFileSync('xml.xml');
     const parsedXml = await xml2js.parseStringPromise(xml);
     
     console.log(parsedXml);
-
 }
 
 main();
